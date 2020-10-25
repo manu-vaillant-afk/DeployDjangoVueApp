e58721f46c40c0d9970c01094c8f66e8f21b7a5e
manu-vaillant-afk <manu.vaillant@student.hers.be>
2020-10-25 22:05:10 +0100

    modif uuid

--- todo/core/base/models.py
+++ todo/core/base/models.py
@@ -2,18 +2,15 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, AbstractBaseUser
 import uuid
 
-class MyUUIDModel(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-
 class MyUser(AbstractUser):
-    uid = MyUUIDModel()
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     # todos = models.ForeignKey(Todo, on_delete=models.CASCADE, related_name="owner")
 
     def __str__(self):
         return self.username
 
 class Todo(models.Model):
-    uid = MyUUIDModel()
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     value = models.TextField()
     checked = models.BooleanField(default=False)
     owner = models.ForeignKey(MyUser, related_name='todos', on_delete=models.CASCADE)
